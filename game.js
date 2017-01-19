4772bbac8bb9621b9eb10d234120f5a84d3f5866
Mark Hendriks <mark.hendriks@castawaylabs.com>
2017-01-19 14:24:26 +0100

    Create merged html file, add helpers, fix some misc stuff

--- game.js
+++ game.js
@@ -1,16 +1,43 @@
-/* globals UserData */
+/* globals UserData,StyleHelper,EventHelper,DOMHelper */
 'use strict';
 if (typeof UserData === 'undefined') {
     throw new Error('game.js requires data.js'); 
 }
 
+if (typeof StyleHelper === 'undefined') {
+    throw new Error('game.js requires helpers.js'); 
+}
+
 (function () {
     // Scope all the thing o/
     var user;
-    var doLogin = function (username) {
+    var logOut = function () {
+        StyleHelper.hide('#taskbar');
+        StyleHelper.show('#login-modal');
+        UserData.save(user.name, user);
+    };
+    var logIn = function (username) {
         user = UserData.load(username);
         return !!user;
     };
 
-
+    window.ready(function () {
+        EventHelper.on('#login-modal form', 'submit', (e) => {
+            e.preventDefault();
+            var username = DOMHelper.getProperty('#username', 'value');
+            if (!username) {
+                return false;
+            }
+            if (!logIn(username)) {
+                UserData.save(username, {name: username});
+                if (!logIn(username)) {
+                    throw new Error('Failed to create new user');
+                }
+            }
+            StyleHelper.hide('#login-modal');
+            StyleHelper.show('#taskbar');
+            StyleHelper.set('body', 'backgroundImage', 'url(./images/desktop-bg.jpg)');
+            return false;
+        });
+    });
 }) ();
---
+++ helpers.js
@@ -0,0 +1,110 @@
+'use strict';
+
+window.StyleHelper = (function () {
+    return {
+        hide: function (selector) {
+            document.querySelectorAll(selector).forEach(element => {
+                if (element.style.display === 'none') {
+                    return;
+                }
+                element.setAttribute('data-display', element.style.display);
+                element.style.display = 'none';
+            });
+        },
+        show: function (selector) {
+            document.querySelectorAll(selector).forEach(element => {
+                var old = element.getAttribute('data-display') || '';
+                element.style.display = old;
+            });
+        },
+        set: function (selector, key, value) {
+            document.querySelectorAll(selector).forEach(element => {
+                element.style[key] = value;
+            });
+        },
+
+    };
+}) ();
+
+window.EventHelper = (function () {
+    var select = function (selector, func) {
+        document.querySelectorAll(selector).forEach(element => {
+            func(element);
+        });
+    };
+    return {
+        on: function (selector, event, func) {
+            select(selector, element => {
+                element['on' + event] = func;
+            });
+        },
+        off: function (selector, event) {
+            select(selector, element => {
+                delete element['on' + event];
+            });
+        }
+    };
+}) ();
+
+window.DOMHelper = (function () {
+    var select = function (selector, func) {
+        document.querySelectorAll(selector).forEach(element => {
+            func(element);
+        });
+    };
+    return {
+        getProperty: function (selector, key) {
+            var values = [];
+            select(selector, element => values.push(element[key]));
+            return values.length === 1 ? values[0] : values;
+        },
+        setProperty: function (selector, key, value) {
+            select(selector, element => element[key] = value);
+        },
+        getAttribute: function (selector, key) {
+            var values = [];
+            select(selector, element => values.push(element.getAttribute(key)));
+            return values.length === 1 ? values[0] : values;
+        },
+        setAttribute: function (selector, key, value) {
+            select(selector, element => element.setAttribute(key, value));
+        }
+    };
+}) ();
+
+(function () {
+    var queue = [];
+    var isReady;
+
+    var ready = function () {
+        if (!isReady) {
+            isReady = true;
+            queue.forEach(func => func());
+        }
+    };
+
+    var readyStateChange = function () {
+        if (document.readyState === 'complete') {
+            ready();
+        }
+    };
+
+    if (document.addEventListener) {
+        // first choice is DOMContentLoaded event
+        document.addEventListener('DOMContentLoaded', ready, false);
+        // backup is window load event
+        window.addEventListener('load', ready, false);
+    } else {
+        // must be IE
+        document.attachEvent('onreadystatechange', readyStateChange);
+        window.attachEvent('onload', ready);
+    }
+
+    window.ready = function (func) {
+        if (isReady) {
+            func();
+        } else {
+            queue.push(func);
+        }
+    };
+}) ();
